d6f63a70287cf6d52499fd363760dd42af56ea2e
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-07 15:04:55 -0400

    corsf2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = require('http').Server(app);
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*, https://back-ecomm-mall.vercel.app/api/');
+  res.header('Access-Control-Allow-Origin', 'https://adminstorenodejs.malcolmcordova.com, https://back-ecomm-mall.vercel.app/api/');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
