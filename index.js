a20dd63e50bbce49651b8df849e924173a922880
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-07-03 12:59:58 -0400

    cors

--- index.js
+++ index.js
@@ -17,14 +17,12 @@ const server = require('http').Server(app);
 
 
 
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Access-Control-Allow-Request-Method']
+}));
+app.options('*', cors());
 
 const options = {
     cors: {
